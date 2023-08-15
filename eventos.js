ce7a60086e584f040737ee5024ebfbeae02f5889
emseis <emi.rodriguez9@outlook.com>
2023-08-15 19:31:31 -0300

    Solución al problema de eventos click

--- eventos.js
+++ eventos.js
@@ -2,6 +2,12 @@ function mostrarSaludo(){
     alert("Hola!, Bienvenido");
 }
 
+const botonSaludo = document.getElementById("saludarBtn");
+botonSaludo.addEventListener("click", function(event){
+    event.stopPropagation();
+    mostrarSaludo();
+});
+
 const divSaludo = document.querySelector(".boton-container");
 divSaludo.addEventListener("click", function(){
     alert("Hola!, Soy el div");
